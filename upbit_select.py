fbedf878e82fbadd84860b6b7484062ec3f35b6a
lyw1217 <mvl100d@gmail.com>
2021-03-28 13:01:04 +0900

    upbit 조회

---
+++ upbit_select.py
@@ -0,0 +1,27 @@
+import os
+import jwt
+import uuid
+import hashlib
+from urllib.parse import urlencode
+
+import requests
+
+with open('./file/upbit_aces_key.txt', 'r') as file:
+    access_key = file.read()
+with open('./file/upbit_sec_key.txt', 'r') as file:
+    secret_key = file.read()
+    
+server_url = 'https://api.upbit.com'
+
+payload = {
+    'access_key': access_key,
+    'nonce': str(uuid.uuid4()),
+}
+
+jwt_token = jwt.encode(payload, secret_key)
+authorize_token = 'Bearer {}'.format(jwt_token)
+headers = {"Authorization": authorize_token}
+
+res = requests.get(server_url + "/v1/accounts", headers=headers)
+
+print(res.json())
