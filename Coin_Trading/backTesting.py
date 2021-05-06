382076ef6f64aeb894e9e905192e00236be435d6
lyw1217 <mvl100d@gmail.com>
2021-05-06 17:46:59 +0900

    back test 보완

--- Coin_Trading/backTesting.py
+++ Coin_Trading/backTesting.py
@@ -1,15 +1,27 @@
 import pyupbit
+import pandas
 import numpy as np
+import time
 
-df = pyupbit.get_ohlcv("KRW-BTC", count=7)
-df['range'] = (df['high'] - df['low']) * 0.5
-df['target'] = df['open'] + df['range'].shift(1)
+date = None
+dfs = [ ]
 
-fee = 0.05
-df['ror'] = np.where(df['high'] > df['target'],
-                     df['close'] / df['target'] - fee,
-                     1)
-df['hpr'] = df['ror'].cumprod()
-df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
-print("MDD(%): ", df['dd'].max())
+for i in range(20) :     # 1회 조회당 200개씩
+    df = pyupbit.get_ohlcv("KRW-ETH", interval="minute30", to=date)
+    dfs.append(df)
+
+    date = df.index[0]
+    time.sleep(0.5)
+
+df = pandas.concat(dfs).sort_index()
+
+df['ma_15'] = df['close'].rolling(15).mean()
+df['ma_50'] = df['close'].rolling(50).mean()
+
+df['cond_1'] = np.where( ( (df['ma_15'] >= df['ma_50']) ), 0, 1 )
+df['cond_2'] = np.where( ( (df['ma_15'].shift(-1) < df['ma_50'].shift(-1)) ), 0, 1 )
+
+df['buy'] = np.where( (df['cond_1'] == 1) & (df['cond_2'] == 1) , 1, 0)
+df['sell'] = np.where( (df['cond_1'] == 0) & (df['cond_2'] == 0) , 1, 0)
+
 df.to_excel("dd.xlsx")
